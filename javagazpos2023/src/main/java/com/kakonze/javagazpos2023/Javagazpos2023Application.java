cffe6e6edde1038da63cc09ff909c543a4228562
wisesoft <atmtec@gmail.com>
2022-11-07 15:18:47 -0500

    initial create

---
+++ javagazpos2023/src/main/java/com/kakonze/javagazpos2023/Javagazpos2023Application.java
@@ -0,0 +1,13 @@
+package com.kakonze.javagazpos2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javagazpos2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javagazpos2023Application.class, args);
+	}
+
+}
---
+++ javagazpos2023/src/test/java/com/kakonze/javagazpos2023/Javagazpos2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakonze.javagazpos2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javagazpos2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
